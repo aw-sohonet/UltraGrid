dd06881791d3f12e915fb04989b33b8953879e83
aw-sohonet <andrew.walker@sohonet.com>
2022-06-16 10:03:40 +0100

    Removed some unnecessary log messages

--- src/transmit.cpp
+++ src/transmit.cpp
@@ -845,9 +845,7 @@ void audio_tx_send(struct tx* tx, struct rtp *rtp_session, const audio_frame2 * 
                         if(tx->fec_scheme == FEC_MULT) {
                                 pos = mult_pos[mult_index];
                         }
-
-                        LOG(LOG_LEVEL_VERBOSE) << "FEC SYMBOL SIZE: " << fec_symbol_size << "\n";
-                        LOG(LOG_LEVEL_VERBOSE) << "FEC MULTIPLICATION FACTOR: " << fecMultFactor << "\n";
+                        
                         // If we are using Reed Sollomon then we want to ensure that the packets we
                         // are sending are a multiple of the size of the FEC symbols.
                         if(tx->fec_scheme == FEC_RS && fec_symbol_size * fecMultFactor < tx->mtu - hdrs_len) {
