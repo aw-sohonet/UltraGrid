b9377bc477cc65c886052774b934a6106dd9a1ee
andrew.walker <andrew.walker@sohonet.com>
2022-06-24 15:33:52 +0100

    Removed member function from struct

--- src/audio/types.cpp
+++ src/audio/types.cpp
@@ -642,7 +642,7 @@ tuple<bool, bool> audio_frame2::resample(audio_frame2_resampler & resampler_stat
         return {true, reinitResampler};
 }
 
-int audio_frame::getChannelSampleCount() {
+int audioFrameGetChannelSampleCount(struct audio_frame& audioFrame) {
     // The data length is a result of the bps, channel count, and samples multipled together
-    return this->data_len / this->bps / this->ch_count;
+    return audioFrame.data_len / audioFrame.bps / audioFrame.ch_count;
 }
